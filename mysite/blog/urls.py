5ef72c609b452d0fd0dbb18821730ca3075b6563
kien3052002 <104478648+kien3052002@users.noreply.github.com>
2022-11-26 11:56:03 +0700

    Delete mysite directory

--- mysite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('<slug:post>/', views.post_single, name='post_single'),
-]
